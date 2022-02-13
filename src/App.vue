1b48111186bacffd4625cbe4b1f9d0b2f69f0368
oleksandrmostovyi <63>
2022-02-13 17:38:36 +0100

    New : UI

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,7 @@
 <template>
   <div id="app">
     <h1>Algorithms testing page</h1>
-    <p>1. Выбрать количество повторений</p>
-    <p>2. Нажать на кнопку режима</p>
-    <p>3. Нажать на кнопку Result</p>
-    <input v-model.number="repeat" placeholder="Введите количество повторений">
-    <p>Количество повторений: {{ repeat }}</p>
     <div>
-      <!-- Displaying buttons -->
-      <button
-          v-for="tab in tabs"
-          v-bind:key="tab"
-          v-bind:class="['tab-button', { active: currentTab === tab }]"
-          v-on:click="currentTab = tab"
-        >{{ tab }}</button>
-      <p>Выбранный режим: {{ currentTab }}</p>
       <!-- Running algorithm by pressing on button. Passing the value to selected component -->
       <component v-bind:is="currentTab" v-bind:repeats="repeat" class="tab"></component>
     </div>
@@ -24,65 +11,49 @@
 <script>
 
 //Components import
-import EasyOneTwo from "@/components/EasyOneTwo.vue";
-import EasyOneThree from "@/components/EasyOneThree.vue";
-import EasyOneFour from "@/components/EasyOneFour.vue";
-import EasyOneFive from "@/components/EasyOneFive.vue";
-import EasyOneSix from "@/components/EasyOneSix.vue";
-import EasyOneSeven from "@/components/EasyOneSeven.vue";
-import EasyOneEight from "@/components/EasyOneEight.vue";
-import EasyOneNine from "@/components/EasyOneNine.vue";
-import BrotherOne from "@/components/BrotherOne.vue";
-import BrotherTwo from "@/components/BrotherTwo.vue";
-import BrotherThree from "@/components/BrotherThree.vue";
-import BrotherFour from "@/components/BrotherFour.vue";
+import AlgorithmsUI from "@/components/AlgorithmsUI.vue";
 
 
 export default {
   name: 'App',
   data: function () {
     return {
-      currentTab: "EasyOneTwo",
-      tabs: ["EasyOneTwo", "EasyOneThree", "EasyOneFour", "EasyOneFive", "EasyOneSix", "EasyOneSeven", "EasyOneEight", "EasyOneNine",
-      "BrotherOne", "BrotherTwo", "BrotherThree", "BrotherFour"],
+      currentTab: "AlgorithmsUI",
       repeat: 0
     }
   },
-  components: {
-    EasyOneTwo, EasyOneThree, EasyOneFour, EasyOneFive, EasyOneSix, EasyOneSeven, EasyOneEight, EasyOneNine,
-    BrotherOne, BrotherTwo, BrotherThree, BrotherFour
-  }
+  components: { AlgorithmsUI }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  font-size: 24px;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-size: 24px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 
-button {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin-right: 1%;
-  margin-bottom: 1%;
-}
+  button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin-right: 1%;
+    margin-bottom: 1%;
+  }
 
-input {
-  width: 50%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
-}
+  input {
+    width: 50%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+  }
 </style>
---
+++ src/components/AlgorithmsUI.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="algorithms-block">
+    <!-- Choosing block Easy, list of all possible possibilities -->
+    <b-dropdown class="mx-1" variant="outline-danger" right text="Просто">
+      <b-dropdown-item variant="primary" v-for="tab in tabsEasy"
+                       v-bind:key="tab"
+                       v-bind:class="['tab-button', { active: currentTab === tab }]"
+                       v-on:click="currentTab = tab"
+      >{{ tab }}</b-dropdown-item>
+    </b-dropdown>
+    <!-- Choosing block Brother, list of all possible possibilities -->
+    <b-dropdown class="mx-1" variant="outline-success" right text="Брат">
+      <b-dropdown-item variant="primary" v-for="tab in tabsBrother"
+                       v-bind:key="tab"
+                       v-bind:class="['tab-button', { active: currentTab === tab }]"
+                       v-on:click="currentTab = tab"
+      >{{ tab }}</b-dropdown-item>
+    </b-dropdown>
+    <!-- Chosen level and difficulty -->
+    <p>Выбранный режим: {{ currentTab }}</p>
+    <!-- Input fields for repeats amount and speed -->
+    <b-row>
+      <b-col>
+        <b-form-input v-model="repeats" type="number" debounce="500"></b-form-input>
+      </b-col>
+      <b-col>
+        <b-form-input v-model="speed" type="number" step="0.1" debounce="500"></b-form-input>
+      </b-col>
+      <div class="w-100"></div>
+      <b-col>
+        <div class="mt-2">Количество повторений: "{{ repeats }}"</div>
+      </b-col>
+      <b-col>
+        <div class="mt-2">Скорость: "{{ speed }}" сек</div>
+      </b-col>
+    </b-row>
+    <b-button variant="success" v-on:click="startTimer">Generate values</b-button>
+    <div class="generated-value">
+      <p> {{currentValue}} </p>
+    </div>
+    <!-- Progress -->
+    <b-progress :value="sequenceNumber" :max="numberArray.length" show-progress animated></b-progress>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlgorithmsUI",
+  data: function () {
+    return {
+      currentTab: "EasyOneTwo",
+      // Have to change in on our generated array
+      numberArray: [1,2,3,4,5,6,7,8,9,10],
+      // Current value, needed for progress block
+      sequenceNumber: 0,
+      // Value, that will be displayed on Users screen
+      currentValue: null,
+      // Repeats amount from input form
+      repeats: 0,
+      // Speed between digits displaying
+      speed: 0,
+      // All possibilities for each block. Have to be changed later
+      tabsEasy: ["EasyOneTwo", "EasyOneThree", "EasyOneFour", "EasyOneFive", "EasyOneSix", "EasyOneSeven", "EasyOneEight", "EasyOneNine"],
+      tabsBrother: ["BrotherOne", "BrotherTwo", "BrotherThree", "BrotherFour"]
+    }
+  },
+  methods: {
+    // Initializing this.numberArray TODO
+    getNewNumbersArray: function() {},
+
+    // Starting our timer, has to update this.currentValue each this.speed * 1000 milliseconds
+    startTimer: function () {
+      this.sequenceNumber = 0;
+      for(let number of this.numberArray) {
+        setTimeout(this.updateValue(), this.speed * 1000);
+        this.sequenceNumber = this.numberArray.indexOf(number) + 1;
+        console.log(this.sequenceNumber)
+      }
+    },
+    updateValue: function () { this.currentValue = this.numberArray[this.sequenceNumber]; }
+  }
+}
+</script>
+
+<style scoped>
+
+  #algorithms-block {
+    margin: auto;
+    max-width: 80%;
+    max-height: 80%;
+  }
+
+</style>
